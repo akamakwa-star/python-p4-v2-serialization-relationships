d795401c44bd0f05f7929126df13c310a1d06a3e
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2026-01-15 21:28:11 +0300

    lab

--- server/models.py
+++ server/models.py
@@ -1,54 +1,35 @@
-# server/models.py
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+class Animal:
+    def __init__(self, name=None, species=None):
+        self.name = name
+        self.species = species
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
-
-metadata = MetaData(naming_convention=convention)
-
-db = SQLAlchemy(metadata=metadata)
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "species": self.species
+        }
 
 
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
+class Enclosure:
+    def __init__(self, name=None, capacity=None):
+        self.name = name
+        self.capacity = capacity
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    birthday = db.Column(db.Date)
-
-    animals = db.relationship('Animal', back_populates='zookeeper')
-
-
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
-
-    id = db.Column(db.Integer, primary_key=True)
-    environment = db.Column(db.String)
-    open_to_visitors = db.Column(db.Boolean)
-
-    animals = db.relationship('Animal', back_populates='enclosure')
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "capacity": self.capacity
+        }
 
 
-class Animal(db.Model):
-    __tablename__ = 'animals'
+class Zookeeper:
+    def __init__(self, name=None, experience_years=None):
+        self.name = name
+        self.experience_years = experience_years
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    species = db.Column(db.String)
-
-    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
-    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
-
-    enclosure = db.relationship('Enclosure', back_populates='animals')
-    zookeeper = db.relationship('Zookeeper', back_populates='animals')
-
-    def __repr__(self):
-        return f'<Animal {self.name}, a {self.species}>'
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "experience_years": self.experience_years
+        }
